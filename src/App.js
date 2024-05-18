bc1bc502a7dded5b15f035ff220fe2a1cfbdbdb7
victor-levus <95586250+victor-levus@users.noreply.github.com>
2024-05-18 14:08:10 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-5">
+      {/* Album art */}
+      <div className="flex justify-center album-art">
+        <img
+          src="https://streamdata.radiohitwave.com/api/placeholder-cover.jpg"
+          alt=""
+          width={"100%"}
+        />
+      </div>
+
+      {/* Current Song Playing Title */}
     </div>
   );
 }
